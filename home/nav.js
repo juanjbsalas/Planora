f5de48780e7111bb7172d7fec7648e11d638da6f
Juan Salas <142240574+juanjbsalas@users.noreply.github.com>
2025-07-11 14:03:51 -0400

    Almost finalized navbar and homepage. (#9)
    * created skeleton
    
    * added bootstrap
    
    * added navbar skeleton
    
    * center text on navbar, include logo
    
    * added header
    
    * added main body
    
    * fixed button and h1 height
    
    * fixed navbar and heading alignment
    
    * edits
    
    * edits
    
    * edits2
    
    * more edits
    
    * move index to root folder, change name from index to home.html
    
    * edit name of home.html
    
    * more more edits
    
    * changed name to global voyage
    
    * fixed footer
    
    * delete home.html
    
    * edits
    
    * added contact link to contact button
    
    * edit footer
    
    * move app.css to home and create asset folder
    
    * create new nav bar with tailwind, added video background, create hamburger menu
    
    * made menu only on hald of the page
    
    * replace link tag with newer icons, fixed toggle hamburguer menu button
    
    * fixed hamburguer bar
    
    * delete unecessary comments
    
    * delete old index folder
    
    * made global voyage appear on hamburguer menu
    
    * changed global voyage font size
    
    * added script so that nav bar is accross all pages
    
    * added more links to navbar
    
    * fixed hamburguer button clickability

---
+++ home/nav.js
@@ -0,0 +1,24 @@
+fetch('home/nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem, oldelem);
+
+    // Event listener for hambuger button click
+    const toggleButton = document.getElementById('menu-toggle');
+    const menuIcon = document.getElementById('menu-icon');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    toggleButton.addEventListener('click', function(){
+        mobileMenu.classList.toggle('hidden');
+
+        if(menuIcon.classList.contains('fa-bars')) {
+            menuIcon.classList.replace('fa-bars', 'fa-xmark');
+        } else {
+            menuIcon.classList.replace('fa-xmark', 'fa-bars');
+        }
+    });
+
+});
